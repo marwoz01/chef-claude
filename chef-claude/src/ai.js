e143a9ec39f6de4a0b3532fd8699fa9a040c4bb2
Marcel Wozniak <marwozniak01@gmail.com>
2025-08-08 21:49:52 +0200

    update ai.js

--- chef-claude/src/ai.js
+++ chef-claude/src/ai.js
@@ -5,7 +5,7 @@ You are an assistant that receives a list of ingredients that a user has and sug
 `;
 
 const anthropic = new Anthropic({
-  apiKey: process.env.ANTHROPIC_API_KEY,
+  //   apiKey: process.env.ANTHROPIC_API_KEY,
   dangerouslyAllowBrowser: true,
 });
 
